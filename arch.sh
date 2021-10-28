0205aba1cd0e64a599d9f337eddbce5dfa99f5b0
Pedro <pether.1994@gmail.com>
2021-10-27 20:16:26 -0500

    next step

--- arch.sh
+++ arch.sh
@@ -12,7 +12,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font sudo qtile xorg gdm alacritty gnome nitrogen thunar picom
+pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font sudo qtile xorg gdm alacritty gnome nitrogen thunar picom neovim zsh zsh-completions firefox curl wget nnn code sxiv git
 
 grub-install --target=i386-pc /dev/sda 
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -30,13 +30,18 @@ systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
+sudo pacman -S --needed base-devel
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://aur.archlinux.org/yay.git
+
 useradd -m user-p
 echo user-p:password | chpasswd
 usermod -aG libvirt user-p
 
 echo "user-p ALL=(ALL) ALL" >> /etc/sudoers.d/user-p
 
-
-printf "\e[1;32mDone! ok.\e[0m"
+printf "\e[1;32m N.\e[0m"
 
 
---
+++ yayReps.sh
@@ -0,0 +1,8 @@
+
+yay -S brave-bin telegram-desktop zoom postman yarn whatsapp-nativefier goop neofetch onlyoffice-bin google-keep-nativefier ttf-meslo-nerd-font- powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-systax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh_my_zsh/custom}/themes/powerlevel10k
