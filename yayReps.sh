e2ee3f5d667c203426536afcf37e269615005ac3
User-P <57698012+User-P@users.noreply.github.com>
2022-01-29 02:17:08 -0600

    fix: syntax

--- yayReps.sh
+++ yayReps.sh
@@ -1,7 +1,7 @@
 
 yay -S brave-bin telegram-desktop zoom postman yarn whatsapp-nativefier goop neofetch onlyoffice-bin google-keep-nativefier ttf-meslo-nerd-font- powerlevel10k mariadb
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-systax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
